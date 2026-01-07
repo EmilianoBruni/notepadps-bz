eb00fadbc07ca7cd0f0540aaa43e9adf8c64f6ba
Emiliano Bruni <info@ebruni.it>
2026-01-07 21:07:41 +0100

    fix: restructure next.config.mjs for clarity and organization

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
- 
-}
+    output: "static",
+    distDir: "notepadps-bz",
+    webpack: (config, options) => {
+        // per verificare che watch guardi le directory giuste
+        // strace -fe inotify_add_watch npm run dev
+        // remove node_modules
+        config.watchOptions.ignored.push("**/node_modules");
+        // don't know why but it try to watch /ws/folder_name/ws/foldername too
+        config.watchOptions.ignored.push("**/ws");
+        // don't know but it watch .. too so don't create app in /first_level dir
+        // /app is bad
+        return config;
+    },
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+    images: {
+        unoptimized: true,
+    },
+};
 
-export default nextConfig
+export default nextConfig;
